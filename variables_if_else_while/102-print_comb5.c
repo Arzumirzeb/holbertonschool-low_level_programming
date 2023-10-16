ac1ff485fdbab22303ffa2228236e4a729b21925
root <arzumirzeb03@gamil.com>
2023-10-16 13:12:18 -0700

    102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, f, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (f = i; f <= 57; f++)
+			{
+				for (k = j + 1; k <= 57; k++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(f);
+					putchar(k);
+					if (i < 57 || j < 56 || f < 57 || k < 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+					putchar('\n');
+					return (0);
+}
