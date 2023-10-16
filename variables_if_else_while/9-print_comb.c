debaafb9a73e00547fb33263b386708585ba4776
root <arzumirzeb03@gamil.com>
2023-10-16 08:17:27 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+	char b = '0';
+
+	while (x < 10)
+	{
+		putchar(b);
+		b++;
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
