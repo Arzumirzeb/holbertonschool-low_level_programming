53846f07358854a274648768bf3ccea2b4d08b3b
root <arzumirzeb03@gamil.com>
2023-10-16 06:59:59 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char b = '0';
+
+	for (a = 0; a < 10 ; a++)
+	{
+		putchar(b);
+		b++;
+	}
+	return (0);
+}
