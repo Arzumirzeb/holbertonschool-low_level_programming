35a9d915f824131f8ea65273e42a8f0c42c4b93f
root <arzumirzeb03@gamil.com>
2023-10-17 06:05:43 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char a[] = "_putchar\n";
+
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		_putchar(a[i]);
+		i++;
+	}
 	return (0);
 }
