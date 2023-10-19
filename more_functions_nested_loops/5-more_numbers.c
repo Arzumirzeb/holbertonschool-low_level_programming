0095f4709d9912040f0c3e06bb86c4a8c8175637
root <arzumirzeb03@gamil.com>
2023-10-19 09:02:32 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - check the code.
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int n, a;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 10)
+			{
+				_putchar(a / 10 + '0');
+			}
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+}
