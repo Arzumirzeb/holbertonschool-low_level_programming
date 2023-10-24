1b94e044d56fc311626406da767653b3e4869e6b
root <arzumirzeb03@gamil.com>
2023-10-24 13:08:14 -0700

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncpy - check the code
+ * @dest: pointer
+ * @src: pointer
+ * @n: variable
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; *(dest + i) != 0; i++)
+	{
+	}
+	for (; *(src + i) != 0; i++)
+	{
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	}
+	*(dest + i) = 0;
+	return (dest);
+}
