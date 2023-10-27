6cea59b0566b4cb34cb698c551591346ca2f62a3
root <arzumirzeb03@gamil.com>
2023-10-27 15:19:34 -0700

    7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code
+ * @a: integer
+ * @size: variable
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j = 0;
+	int sum_d1 = 0;
+	int sum_d2 = 0;
+
+	while (i <= size * size)
+	{
+		sum_d1 = sum_d1 + *(a + i);
+		i = i + size + 1;
+	}
+	while (j < size * size - 1)
+	{
+		sum_d2 = sum_d2 + *(a + j);
+		j = j + size - 1;
+	}
+	printf("%d, %d\n", sum_d1, sum_d2);
+}
