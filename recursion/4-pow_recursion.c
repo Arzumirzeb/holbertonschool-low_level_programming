3ca543f60e9d3fa7cad6ad182f66e9235e28a482
root <arzumirzeb03@gamil.com>
2023-10-30 12:23:11 -0700

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,11 +3,22 @@
 
 /**
  * _pow_recursion - check the code
- * @x: variable
- * @y: variable
+ * @x: base
+ * @y: power
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return(x * _pow_recursion(x, y - 1));
+	}
 }
