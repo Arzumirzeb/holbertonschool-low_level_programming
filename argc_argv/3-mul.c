a84d658bb105249e8db2616e2071055e34094ba6
root <arzumirzeb03@gamil.com>
2023-11-03 07:59:36 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 	if (argc <= 2)
 	{
 		printf("error\n");
-		return 1;
+		return (1);
 	}
 	else
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
-	 return (0);
+	return (0);
 
 }
