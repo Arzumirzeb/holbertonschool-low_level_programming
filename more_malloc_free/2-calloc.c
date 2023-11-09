d2909a64e1ff363e16bc9bb64d5ad577ba1b85c0
root <arzumirzeb03@gamil.com>
2023-11-09 14:21:29 -0800

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - prints buffer in hexa
+ * @nmemb: nmemb
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arzu;
+	unsigned int i;
+	
+	if(nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arzu = malloc(sizeof(char) * nmemb * size);
+	if (arzu == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb * size; i++)
+	{
+		arzu[i] = 0;
+	}
+	return (arzu);
+}
