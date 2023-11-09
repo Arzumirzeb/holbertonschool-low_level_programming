720037b3d12463b40994ad12511b5dc5685c01c1
root <arzumirzeb03@gamil.com>
2023-11-09 12:26:31 -0800

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * *malloc_checked- check the code
+ * @b: b
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return(p);
+}
