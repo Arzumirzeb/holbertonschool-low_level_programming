72a8823998443d44d4d462da924babc53dfd6d96
root <arzumirzeb03@gamil.com>
2023-11-20 01:15:44 -0800

    1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code
+ * @d:pointer
+ *
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+}
