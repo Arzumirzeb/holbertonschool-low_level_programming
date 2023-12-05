268036e47a66beb3508ed378037cc6020f8c8289
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-05 05:15:11 -0800

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - check the code
+ * @h: h
+ * Return: Always EXIT_SUCCESS.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
