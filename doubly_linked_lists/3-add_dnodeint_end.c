119f14e99976e0d7553c58734dab8119bd474ad9
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-05 06:46:36 -0800

    add end of list

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - check the code
+ * @head: head
+ * @n: integer
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;
+
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->next = new;
+		new->prev = last;
+	}
+	return (*head);
+}
