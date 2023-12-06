ebf20397dfcd1de0372ee06fd5b469a20d5b0c1d
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-06 03:11:33 -0800

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - check the code
+ * @head: head
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->next);
+		free(temp);
+	}
+	free(head);
+}
