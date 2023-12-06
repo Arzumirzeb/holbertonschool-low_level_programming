18f287bbf34dccc532673157cc758b7220297910
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-06 04:33:39 -0800

    5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - check the code
+ * @head: head
+ * @index: index
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = head;
+	unsigned int count = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head)
+	{
+		if (count == index - 1)
+		{
+			return (temp->next);
+		}
+		count++;
+		temp = temp->next;
+	}
+	return (head);
+}
