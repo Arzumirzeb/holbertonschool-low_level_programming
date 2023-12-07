4d67723f051b53ca8fa52d350f52cf1a1fa813f5
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-07 11:35:29 -0800

    7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,23 +4,6 @@
 #include "lists.h"
 
 /**
- * dlistint_len - check the code
- * @h: h
- * Return: Always EXIT_SUCCESS.
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t count = 0;
-
-	while (h)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
-}
-
-/**
  * insert_dnodeint_at_index - check the code
  * @h: head
  * @idx: index
@@ -29,40 +12,44 @@ size_t dlistint_len(const dlistint_t *h)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
+	dlistint_t *temp = *h;
 	dlistint_t *new;
-	unsigned int count = 0, len;
+	unsigned int count = 0;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 	{
 		return (NULL);
 	}
-	new->n = n;
-	len = dlistint_len(*h);
-	if (!*h && idx == 0)
+	if (new)
 	{
-		*h = new;
-	}
-	else if (idx == 0)
-	{
-		new->next = *h;
-		(*h)->prev = new;
-		*h = new;
-		return (new);
-	}
-	else if (idx < len - 1)
-	{
-		temp = *h;
-		while (idx - 1 > count)
+		new->n = n;
+		if (idx == 0)
 		{
-			count++;
+			if (*h)
+			{
+				new->next = *h;
+				(*h)->prev = new;
+			}
+			*h = new;
+			return (new);
+		}
+	while (idx - 1 > count && temp)
+	{
 			temp = temp->next;
+			count++;
+	}
+	if (temp)
+		{
+		if (temp->next)
+		{
+		temp->next->prev = new;
 		}
 		new->next = temp->next;
 		new->prev = temp;
 		temp->next = new;
 		return (new);
+		}
 	}
 	return (NULL);
 }
