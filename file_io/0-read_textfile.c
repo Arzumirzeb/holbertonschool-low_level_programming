7331fb6f0643c8b0a11aeb960dab39168c35ddb3
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-11 05:56:28 -0800

    0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - check the code
+ * @filename: filename
+ * @letters: letters
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+	if (fd == -1 || r == -1 || w == -1)
+	{
+		free(buf);
+		return (0);	
+	}
+	free(buf);
+	close(fd);
+	return (w);
+}
