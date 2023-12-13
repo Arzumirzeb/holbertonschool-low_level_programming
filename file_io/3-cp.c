1b84bd9f8599f7b1bd88f8a79d77e14b9463ec91
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-13 13:13:29 -0800

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,9 +15,7 @@ void copy_file(const char *file1, const char *file2)
 
 	from = open(file1, O_RDONLY);
 	if (file1 == NULL || from == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1), exit(98);
-	}
 	to = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to == -1)
 	{
