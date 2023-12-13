4c40211e37e4058caa79b2aa774c0256f565a690
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-13 04:30:59 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDONLY | O_CREAT | O_APPEND);
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND);
 	if (!text_content)
 	{
 		close(fd);
