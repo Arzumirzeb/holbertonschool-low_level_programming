c97ddb33cad7304671ed69a9d355bef8b7a600f4
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-13 13:39:03 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,16 +16,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | S_IRUSR | S_IWUSR);
-	if (!text_content)
-	{
-		close(fd);
-		return (1);
-	}
-	w = write(fd, text_content, strlen(text_content));
-	if (fd == -1 || w == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC , 0600);
+	if (fd == -1)
 		return (-1);
+	if (text_content)
+	{
+		w = write(fd, text_content, strlen(text_content));
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
-
 }
