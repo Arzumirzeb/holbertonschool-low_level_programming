fa27d14907f7c9a020feafa0b918c3b2382c7ad4
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-15 03:24:02 -0800

    2

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * int key_index - check the code
+ * @key: key
+ * @size: size
+ * Return: Always EXIT_SUCCESS.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb(key) % size);
+}
