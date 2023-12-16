ec24b8a1e9fdc12eecf343e68ad2a4ad9f5c5a4d
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-16 13:37:21 -0800

    5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (temp)
 		{
 			printf("%s'%s': '%s'", v, temp->key, temp->value);
-			v = ",";
+			v = ", ";
 			temp = temp->next;
 		}
 		index++;
