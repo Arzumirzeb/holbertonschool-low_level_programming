4d94bfe11c397d838fade00842021d604bbd13de
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-16 13:07:10 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -18,10 +18,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
-	while (temp && strcmp(temp->key, key) == 0)
+	while (temp)
 	{
-		return (temp->value);
-		temp = temp->next;
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+			temp = temp->next;
+		}
 	}
 	return (NULL);
 }
