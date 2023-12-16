2880c30c76763b31cc3e295d869c08ba33c2488f
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-16 12:58:21 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,12 +11,14 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *temp;
 	unsigned long int index;
 
 	if (!ht || !key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[index] && !strcmp(ht->array[index]->key, key))
+	temp = ht->array[index];
+	while (temp && !strcmp(temp->key, key))
 	{	return (ht->array[index]->value);
 		ht->array[index] = ht->array[index]->next;
 	}
