b46c7edb56c71ea99614e3d2418310220f571645
arzumirzeb <arzumirzeb03@gmail.com>
2023-12-16 12:23:44 -0800

    3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,15 +3,36 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
+
 /**
  * hash_table_set - check the code
+ * @ht: ht
  * @key: key
  * @value: value
  * Return: Always EXIT_SUCCESS.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *table = create(key, value);
+	unsigned long int i;
+	hash_node_t *new;
 
-	int index = hash_
+	if (!ht || !key || !value)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	for (i = 0; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = strdup(value);
+			return (1);
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	ht->array[i] = new;
+	return (1);
 }
